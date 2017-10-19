f6f82ee0994ee242205ba67dfa2220d3fbaeed00
ChoiSeoYoung <32355047+ChoiSeoYoung@users.noreply.github.com>
2017-10-20 00:11:13 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function raindrop(x, y, vy, sz, c) {  // Make a new object by new function, rai
 function setup() {     
   createCanvas(windowWidth, windowHeight);  
   noStroke();    
-  Fill(100);
+  fill(100);
   ellipse(mouseX, mouseY, 200, 400);
   var i;  
   for (i=0; i<max_raindrops; i++) {         
