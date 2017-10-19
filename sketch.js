04fe25142156fe0d6cc290806e6317c5f42a4170
ChoiSeoYoung <32355047+ChoiSeoYoung@users.noreply.github.com>
2017-10-19 22:51:48 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,37 @@
-var max_raindrops = 100; var raindrops = []; //arrayfunction raindrop(x, y, vy, sz, c) {  // Make a new object by new function, Planet  this.x = x;     this.y = y;  this.vy = vy;  //velocity y  this.sz = sz; //size    this.c = c; //color  this.move = function() {    this.y += this.vy;        if (this.y<0 || this.y>windowHeight) this.y = 0;    //to change the direction of circle when it goes out of frame  }  this.render = function() {    noStroke();    fill(this.c);    ellipse(this.x, this.y, 2, this.sz);     }}
-function setup() {     createCanvas(windowWidth, windowHeight);  var i;  for (i=0; i<max_raindrops; i++) {         raindrops[i] = new raindrop(      random(0, windowWidth), random(0,windowHeight),      random(10,50), random(30,100),      color(255));       }}
-function draw() {     background(200,200,200);  var i;  for (i=0; i<max_raindrops; i++) {         raindrops[i].move(); //call raindrops[0],raindrops[1], ... and make them move    raindrops[i].render();  }}//if raindrop meet ellipse, then it goes upfunction hand(p,q,r) {  this.p = mouseX;  this.q = mouseY;  this.r = r;  this.arm = function() {    noStroke();    fill(this.r);    ellipse(this.p, this.q, 10, 30);  }}
+var max_raindrops = 100; 
+var raindrops = []; //array
+function raindrop(x, y, vy, sz, c) {  // Make a new object by new function, Planet
+  this.x = x;     
+  this.y = y;  
+  this.vy = vy;  //velocity y  
+  this.sz = sz; //size    
+  this.c = c; //color  
+  this.move = function() {
+    this.y += this.vy;        
+    if (this.y<0 || this.y>windowHeight) this.y = 0; //to change the direction of raindrop when it goes out of frame  
+  }  
+  this.render = function() {    
+    noStroke();    
+    fill(this.c);    
+    ellipse(this.x, this.y, 2, this.sz);     
+  }
+}
+function setup() {     
+  createCanvas(windowWidth, windowHeight);  
+  var i;  
+  for (i=0; i<max_raindrops; i++) {         
+    raindrops[i] = new raindrop (
+      random(0, windowWidth), random(0,windowHeight),      
+      random(10,50), random(30,100), color(255));       
+  }
+}
+function draw() {     
+  background(200,200,200);  
+  var i;  
+  for (i=0; i<max_raindrops; i++) {         
+    raindrops[i].move(); //call raindrops[0],raindrops[1], ... and make them move    
+    raindrops[i].render();  
+  }
+}
+//if raindrop meet ellipse, then it goes up
+//function hand(p,q,r) {  this.p = mouseX;  this.q = mouseY;  this.r = r;  this.arm = function() {    noStroke();    fill(this.r);    ellipse(this.p, this.q, 10, 30);  }}
