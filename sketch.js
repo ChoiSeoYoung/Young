c3fd0927e99adb98885208ef0b1694f291fed0e6
ChoiSeoYoung <32355047+ChoiSeoYoung@users.noreply.github.com>
2017-10-25 22:34:07 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,19 +6,19 @@ function setup() { 
 function draw() {  
   background(250, 250, 250);  
   randomSeed(0);  
-  var x, y; 
-  var delta = map(mouseX, 0, windowWidth, 10, 100);  
+  var x, y, r; 
+  var delta = 50;
+  var bsprob = map(mouseX, 0, windowWidth, 0,1);  
   var thickness = map(mouseY,0,windowHeight,6,30);  
   for (var x=0; x<windowWidth; x+=delta) {   
     for (var y=0; y<windowHeight; y+=delta){      
-      var r1 = random(0,1);       
-      var r2 = random(0,1);            
-      if (r1<0.5){        
+      r = random(0,1);                   
+      if (r<bsprob){        
         stroke(120,40,200);        
         strokeWeight(thickness);       
         line(x, y, x+delta, y+delta);      
-      }      
-      if (r2<0.5){        
+      } 
+      else (r2<0.5){        
         strokeWeight(2);        
         stroke(0);        
         line(x+delta, y, x, y+delta);              
