3e965bed20a7593cce0ca70ded61cbc4c8384eba
ChoiSeoYoung <32355047+ChoiSeoYoung@users.noreply.github.com>
2017-10-25 14:50:38 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,38 +1,39 @@
-var max_raindrops = 100; 
-var raindrops = []; //array
-function raindrop(x, y, vy, sz, c) {  // Make a new object by new function, raindrop
-  this.x = x;     
+var max_planets  = 100;
+var planets = [];
+function planet(x,y,vx,vy,sz,c) {  
+  this.x = x;  
   this.y = y;  
-  this.vy = vy;  //velocity y  
-  this.sz = sz; //size    
-  this.c = c; //color  
-  this.move = function() {
-    this.y += this.vy;        
-    if (this.y<0 || this.y>windowHeight) this.y = 0; //to change the direction of raindrop when it goes out of frame  
+  this.vx = vx;  
+  this.vy = vy; 
+  this.sz = sz;  
+  this,c = c;  
+  this.move = function() {    
+    this.x += this.vx;    
+    this.y += this.vy;    
+    if (this.x<0 || this.x>windowWidth) this.vx = -this.vx;    
+    if (this.y<0 || this.y>windowHeight) this.vy = =this.vy;  
   }  
   this.render = function() {    
     noStroke();    
-    fill(this.c);    
-    ellipse(this.x, this.y, 2, this.sz);
+    fill(this.c);   
+    ellipse(this.x, this.y, this.sz, this.sz)  
   }
 }
-function setup() {     
+function setup () {  
   createCanvas(windowWidth, windowHeight);  
   var i;  
-  for (i=0; i<max_raindrops; i++) {         
-    raindrops[i] = new raindrop (
-      random(0, windowWidth), random(0,windowHeight),      
-      random(10,50), random(30,100), color(255));       
+  for (i=0; i<max_planets; i++) {    
+    planets[i] = new planet (      
+      random(0,windowWidth), random(0,windowHeight),      
+      random(-2,2), random(-2,2), random(10,100), color((0,255),0,0)    
+    )        
   }
 }
-
-function draw() {     
-  background(200,200,200);  
+function draw () {  
+  background(255);  
   var i;  
-  for (i=0; i<max_raindrops; i++) {         
-    raindrops[i].move(); //call raindrops[0],raindrops[1], ... and make them move    
-    raindrops[i].render();  
-  }
+  for (i=0; i<max_planets; i++) {    
+    planets[i].move();   
+    planets[i].render();
+  }
 }
-
-//if raindrop meet ellipse, then it goes up
