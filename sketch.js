c29c11a1c0905ea904d7b042728eb77a88d969b7
ChoiSeoYoung <32355047+ChoiSeoYoung@users.noreply.github.com>
2017-10-25 15:09:37 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,39 +1,29 @@
-var max_planets  = 100;
-var planets = [];
-function planet(x,y,vx,vy,sz,c) {  
-  this.x = x;  
-  this.y = y;  
-  this.vx = vx;  
-  this.vy = vy; 
-  this.sz = sz;  
-  this.c = c;  
-  this.move = function() {    
-    this.x += this.vx;    
-    this.y += this.vy;    
-    if (this.x<0 || this.x>windowWidth) this.vx = -this.vx;    
-    if (this.y<0 || this.y>windowHeight) this.vy = -this.vy;  
-  }  
-  this.render = function() {    
-    noStroke();    
-    fill(this.c);   
-    ellipse(this.x, this.y, this.sz, this.sz)  
-  }
-}
-function setup () {  
+function setup() {  
   createCanvas(windowWidth, windowHeight);  
-  var i;  
-  for (i=0; i<max_planets; i++) {    
-    planets[i] = new planet (      
-      random(0,windowWidth), random(0,windowHeight),      
-      random(-2,2), random(-2,2), random(10,100), color(random(0,255),0,0)    
-    )        
+  background(250, 0, 0);
+}
+
+function draw() {  
+  background(250, 250, 250);  
+  randomSeed(0);  
+  var x, y; 
+  var delta = map(mouseX, 0, windowWidth, 10, 100);  
+  var thickness = map(mouseY,0,windowHeight,6,30);  
+  for (var x=0; x<windowWidth; x+=delta) {   
+    for (var y=0; y<windowHeight; y+=delta){      
+      var r1 = random(0,1);       
+      var r2 = random(0,1);            
+      if (r1<0.5){        
+        stroke(120,40,200);        
+        strokeWeight(thickness);       
+        line(x, y, x+delta, y+delta);      
+      }      
+      if (r2<0.5){        
+        strokeWeight(2);        
+        stroke(0);        
+        line(x+delta, y, x, y+delta);              
+      }    
+    }    
   }
 }
-function draw () {  
-  background(255);  
-  var i;  
-  for (i=0; i<max_planets; i++) {    
-    planets[i].move();   
-    planets[i].render();
-  }
-}
+
