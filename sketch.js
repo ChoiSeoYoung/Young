3d22f06aab9d73b2c12bd6925e2f2fad3b8b3f0e
ChoiSeoYoung <32355047+ChoiSeoYoung@users.noreply.github.com>
2017-10-26 12:58:20 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,34 +1,33 @@
-float[] cx;
-float[] cy;
-int N; 
-
-void setup() {  
-  frameRate(10);  
-  size(600,600);  
-  background(200, 200, 200);  
-  position();
+function setup() {  
+  createCanvas(windowWidth, windowHeight);
 }
-void position() {  
-  N = 100;  
-  cx = new float[N];  
-  cy = new float[N];  
-  for (int i=0; i<N; i++) {    
-    cx[i] = random(0, width);    
-    cy[i] = random(0, height);  
+var mode = 'RECT';
+function draw() {  
+  background(0,0,0);  
+  randomSeed(0);  
+  stroke(255);
+  var x, y, r;  
+  var delta = map(mouseX, 0, windowWidth, 10, 100);  
+  for (y=0; y<windowHeight; y+=delta) {   
+    for (x=0; x<windowWidth; x+=delta) {      
+      r = random(0,1);     
+      if (r<0.5) {        
+        fill(0,0,random(0,255))        
+        switch(mode) {          
+          case 'RECT' : rect(x,y,delta,delta);          
+          break;          
+          case 'ELLIPSE' : ellipse(x,y,delta,delta);          
+          break;        
+        }      
+      }    
+    } 
   }
-}  
-//for each frame
-void draw() {  
-  background(200);  
-  fill(255);  
-  stroke(250);  
-  float size = random(5, 15);  
-  for (int i=0; i<N; i++) {    
-    cx[i] = cx[i] + random(-10, 10);    
-    cy[i] = cy[i] + random(0, 10);    
-    if (cy[i]>height) cy[i]=0;    
-    ellipse(cx[i], cy[i], size, size);  
+}
+function keyPressed() {  
+  switch(key) {    
+    case '1' : mode = 'RECT';    
+    break;    
+    case '2' : mode = 'ELLIPSE';
+    break; 
   }
-} 
-
-
+}
